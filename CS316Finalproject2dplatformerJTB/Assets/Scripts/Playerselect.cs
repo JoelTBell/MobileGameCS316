e787fc1366393f5fb96312303a508cad455e9523
JoelTBell <93441802+JoelTBell@users.noreply.github.com>
2022-03-29 12:22:29 -0500

    PlayerButtons2(3/29)

--- CS316Finalproject2dplatformerJTB/Assets/Scripts/Playerselect.cs
+++ CS316Finalproject2dplatformerJTB/Assets/Scripts/Playerselect.cs
@@ -18,6 +18,8 @@ public class Playerselect : MonoBehaviour
     void Start()
     {
         Cma = Camera.main;
+        Player2LB.SetActive(false);
+        Player2RB.SetActive(false);
     }
 
    private void Update()
@@ -35,10 +37,10 @@ public class Playerselect : MonoBehaviour
         if(characterIndex > 1) //Or however many characters you want to switch between, I did 2 because I have 3 characters and start at 0
         { 
             characterIndex = 0;
-           Player1LB.SetActive(false);
-           Player1RB.SetActive(false);
-           Player2LB.SetActive(true);
-           Player2RB.SetActive(true); 
+           Player1LB.SetActive(!Player1LB.activeSelf);
+           Player1RB.SetActive(!Player1RB.activeSelf);
+           Player2LB.SetActive(!Player2LB.activeSelf);
+           Player2RB.SetActive(!Player2RB.activeSelf); 
         }
         Cma.transform.parent = Players[characterIndex].transform;
         Cma.transform.localPosition = new Vector3(0.03f, 2.37f, -10f);
