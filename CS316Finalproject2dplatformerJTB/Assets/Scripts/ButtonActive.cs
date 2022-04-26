95299a4f18bf63fa0d4d08c5e64e80d4fb003f17
JoelTBell <93441802+JoelTBell@users.noreply.github.com>
2022-04-26 10:28:37 -0500

    I require much Help(4/26)

--- CS316Finalproject2dplatformerJTB/Assets/Scripts/ButtonActive.cs
+++ CS316Finalproject2dplatformerJTB/Assets/Scripts/ButtonActive.cs
@@ -38,42 +38,30 @@ public class ButtonActive : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(other.tag =="Player1")
+        if(collision.GameObject.tag =="BB1")
         {
             Debug.Log("I've found a button");
-            if(other.tag == "BB1")
-            {
-                BA1 = true;
-            }
+            BA1 = true;
         }
         
-        if(other.tag == "Player1")
+        if(collision.GameObject.tag == "BB2")
         {
             Debug.Log("I've found the second button");
-            if(other.tag == "BB2")
-            {
-                BA2 = true;
-            }
+            BA2 = true;
         }
 
-        if(other.tag == "Player2")
+        if(collision.GameObject.tag == "BB3")
         {
             Debug.Log("I've found a button");
-             if(other.tag == "BB3")
-            {
-                BA3 = true;
-            }
+            BA3 = true;
         }
 
-        if(other.tag == "Player2")
+        if(collision.GameObject.tag == "BB4")
         {
             Debug.Log("I've found the first button");
-            if(other.tag == "BB4")
-            {
-                BA4 = true;
-            }
+            BA4 = true;
         }
     }
 
